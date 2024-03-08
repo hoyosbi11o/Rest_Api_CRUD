8694fe8b063a848ee5ad81d96efa4e88058dbd4d
hoyosbi11o <hoyosboli20@gmail.com>
2024-03-07 22:44:41 -0500

    update code

--- dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const authControllers_1 = require("../controllers/authControllers");
-const router = (0, express_1.Router)();
-exports.router = router;
-router.post('/register', authControllers_1.registerCtrl);
-router.post('/login', authControllers_1.loginCtrl);
--- dist/utils/errorHandle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHTTP = void 0;
-const handleHTTP = (res, error) => {
-    res.status(500);
-    res.send({ error });
-};
-exports.handleHTTP = handleHTTP;
